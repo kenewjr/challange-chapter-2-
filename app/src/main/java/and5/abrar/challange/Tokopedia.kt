506747a427a208ab3dc179f15eb4c7e122aaf9d0
kenewjr <kenelewatan@gmail.com>
2022-03-13 19:50:11 +0700

    upload

---
+++ app/src/main/java/and5/abrar/challange/Tokopedia.kt
@@ -0,0 +1,11 @@
+package and5.abrar.challange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tokopedia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tokopedia)
+    }
+}
